074d8587ec0ad67aa242718bdeb2fa9b09589caf
Daniel Escobar <daescobarp@unal.edu.co>
2019-05-08 09:54:56 -0500

    Finished, working and printing

--- mainCvLac.py
+++ mainCvLac.py
@@ -1,5 +1,8 @@
 import urllib.request
 import re
+clear = open("output.txt", "w")
+clear.write("")
+clear.close()
 inputFile = open("input.txt", "r")
 outputFile = open("output.txt", "a+")
 lines = inputFile.readlines()
@@ -8,8 +11,12 @@ for dni in lines:
     htmlSource = str(urllib.request.urlopen(url).read())
     try:
         matchObj = re.search("https://scienti.colciencias.gov.co/cvlac/visualizador/generarCurriculoCv.do%3Fcod_rh%3D([0-9]*)", htmlSource)
-    except:
+        outputFile.write(
+            dni.replace("\n", "") + "," + str(matchObj.group()).replace("%3F", "?").replace("%3D", "=") + "\n")
+        print(dni.replace("\n", "") + "," + str(matchObj.group()).replace("%3F", "?").replace("%3D", "=") + "\n")
+    except Exception:
         outputFile.write(
             dni.replace("\n", "") + "," + "\n")
         continue
-    outputFile.write(dni.replace("\n", "") + "," + str(matchObj.group()).replace("%3F", "?").replace("%3D", "=") + "\n")
+inputFile.close()
+outputFile.close()
